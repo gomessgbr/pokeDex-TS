032567382bfa5cdb67fce9d80833ec9e0e47dab3
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-08 17:31:43 -0300

    feat: create a global style

---
+++ src/styles/global.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle }  from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle `
+
+*{
+   margin : 0;
+   padding: 0;
+   box-sizing:border-box;
+   
+   
+}
+
+#root{
+   margin:0 auto;
+}
+body{
+   background-color:aliceblue;
+   font-family: 'Roboto', sans-serif;
+}
+
+`
