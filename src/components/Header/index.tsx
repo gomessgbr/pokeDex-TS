8a7f879e4cb30cb2980224dbfb417a8f45398df7
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-08 17:47:42 -0300

    lint

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,12 @@
-import { HeaderContainer } from "./styles";
+import { HeaderContainer } from './styles'
 import logoPokeApi from '../../assets/pokeapi-logo.png'
 
-export function Header(){
-    return(
-        <HeaderContainer>
-            <nav>
-                <img src={logoPokeApi} alt="" />
-            </nav>
-        </HeaderContainer>
-    )
+export function Header() {
+  return (
+    <HeaderContainer>
+      <nav>
+        <img src={logoPokeApi} alt="" />
+      </nav>
+    </HeaderContainer>
+  )
 }
