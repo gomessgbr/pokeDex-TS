5ad666515800e9c90e9bb4ab51965ca86f05bbc5
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-01-06 12:15:06 -0300

    feat: add cardComponents on Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,30 @@
+import { Cards } from '../../components/Cards'
 import { Header } from '../../components/Header'
-import { HomeContainer } from './styles'
+import { HomeContainer, HomeContent } from './styles'
 
 export function Home() {
   return (
     <HomeContainer>
       <Header />
-      <h1>Home</h1>
+      <HomeContent>
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+      </HomeContent>
     </HomeContainer>
   )
 }
