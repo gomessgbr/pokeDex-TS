bc32114eac33e5cd549a1c64708f15fe36a2e4bc
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-02-03 19:49:50 -0300

    style: cards in grade

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,4 +1,21 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
+export const HomeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 20rem;
+`
 
-export const HomeContainer = styled.div ``
+export const HomeContent = styled.main`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+
+  width: 100%;
+  max-width: 1120px;
+  margin: 4rem auto;
+  padding: 0 1.5rem;
+`
