c26833989f0ce8cd468e4b064ff0cf5c90cea817
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-02-03 20:15:40 -0300

    style: images

--- src/components/Cards/styles.ts
+++ src/components/Cards/styles.ts
@@ -1,6 +1,20 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const CardsContainer = styled.div`
-  display: grid;
-  grid-template-columns: auto;
-`
+  width: 20rem;
+  height: 20rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+
+  background: red;
+  border: 1px solid;
+`;
+
+export const CardImg = styled.img`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  object-fit: "cover";
+`;
